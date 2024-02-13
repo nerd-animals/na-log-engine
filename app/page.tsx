5ad063499a0853da6d494a85cc782033432c8220
otter <144116866+otterbits@users.noreply.github.com>
2024-02-13 22:49:58 +0900

    ✨ about page 추가 (#9)
    ## summary
    
    about page 추가하였습니다.
    
    - Resolves: #3 
    
    ## PR 유형
    
    어떤 유형인가요? 해당되는 유형에 체크해주세요.
    
    **PR 제목 작성시 '[유형에 맞는 gitmoji] 제목'으로 입력 부탁드립니다.**
    
    - [x] ✨ Feature / 새로운 기능
    - [ ] 🐛 Fix / 버그 수정
    - [ ] 💄 Style / CSS 등 사용자 UI 디자인 변경
    - [ ] ♻️ Refactor / 코드 리팩토링(구조 개선, 디렉토리 변경 등...)
    - [ ] 📝 Docs / 문서 수정
    - [ ] ✅ Test / 테스트 추가, 테스트 리팩토링
    - [ ] 💬 Chore / 그 외, 모든 변경점(Package, Rename, Remove, etc...)
    
    ## 변경 사항
    
    | 기능  | 스크린샷 |
    | :---: | -------- |
    | As-is |![image](https://github.com/nerd-animals/na-log/assets/144116866/eb24530e-3260-44dc-9964-a898f5189da9)|
    | To-be |![image](https://github.com/nerd-animals/na-log/assets/144116866/d9977cdc-ab2e-4589-b737-e18f44085181)![image](https://github.com/nerd-animals/na-log/assets/144116866/4bb99e56-eaaf-4f25-a12d-d840e3ea0221)|
    
    ---
    
    about 페이지를 추가하여 Link로 ``/about`` 페이지로 갈 수 있게 navigation header를 home 화면에 추가하였습니다.
    
    ## PR Checklist
    
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [x] 정상 동작 확인 여부
    - [x] commit message convention 충족 여부

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div className="page-header-wrapper">
+        <header className="page-header">
+          <div className="home-section">
+            <Link href="/">na-log</Link>
+          </div>
+          <div className="category-section">
+            <Link href="/about">about</Link>
+          </div>
+        </header>
+      </div>
       <div className={styles.description}>
         <p>hello, na-log!</p>
       </div>
