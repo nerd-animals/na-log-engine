f883bdf85f4ae55ac419bfea1e4164419a08a701
otter <144116866+otterbits@users.noreply.github.com>
2024-02-14 22:20:40 +0900

    ♻️ header html을 전체 layout.tsx에서 출력되게 이동 (#15)
    ## summary
    
    header html을 전체 layout.tsx에서 출력되게 이동하였습니다.
    
    - Resolves: #(Issue Number)
    
    ## PR 유형
    
    어떤 유형인가요? 해당되는 유형에 체크해주세요.
    
    **PR 제목 작성시 '[유형에 맞는 gitmoji] 제목'으로 입력 부탁드립니다.**
    
    - [ ] ✨ Feature / 새로운 기능
    - [ ] 🐛 Fix / 버그 수정
    - [ ] 💄 Style / CSS 등 사용자 UI 디자인 변경
    - [x] ♻️ Refactor / 코드 리팩토링(구조 개선, 디렉토리 변경 등...)
    - [ ] 📝 Docs / 문서 수정
    - [ ] ✅ Test / 테스트 추가, 테스트 리팩토링
    - [ ] 💬 Chore / 그 외, 모든 변경점(Package, Rename, Remove, etc...)
    
    ## 변경 사항
    
    | 기능  | 스크린샷 |
    | :---: | -------- |
    | As-is |![image](https://github.com/nerd-animals/na-log/assets/144116866/66d8868a-bc0d-41e2-ac07-eaa2f2ca81aa)|
    | To-be |![image](https://github.com/nerd-animals/na-log/assets/144116866/c71253da-0ada-4990-8fe0-c799c1d33fcf)|
    
    ---
    
    header html은 어떤 page에 가더라도 상단에 고정되어 출력됩니다.
    따라서, 전체 layout.tsx에 출력되어야야 합니다.
    
    ## PR Checklist
    
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [x] 정상 동작 확인 여부
    - [x] commit message convention 충족 여부

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,8 @@
-import Link from 'next/link';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className="page-header-wrapper">
-        <header className="page-header">
-          <div className="home-section">
-            <Link href="/">na-log</Link>
-          </div>
-          <div className="category-section">
-            <Link href="/about">about</Link>
-          </div>
-        </header>
-      </div>
       <div className={styles.description}>
         <p>hello, na-log!</p>
       </div>
