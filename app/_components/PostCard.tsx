603a3c3a5dc5564fa214c10fa25c2ce5d56bfd2f
uoohoon <hoonixox@naver.com>
2024-02-17 19:29:37 +0900

    feat: create post card component

---
+++ app/_components/PostCard.tsx
@@ -0,0 +1,18 @@
+export default function PostCard() {
+  return (
+    <div className="post-card-wrapper">
+      <div className="post-card">
+        <div className="post-card-title">
+          test title입니다.
+          <div className="post-card-summary">
+            test summary입니다.
+            <div className="post-card-info">
+              <div className="post-card-date">test date입니다.</div>
+              <div className="post-card-categories">test categories입니다.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
