b6565ae64edb837427108a286e18a93f4797a88c
otter <144116866+otterbits@users.noreply.github.com>
2024-02-17 17:30:38 +0900

    ✨ Bio component 추가 (#21)
    * feat: create bio component
    
    * feat: show bio component in Home.tsx
    
    * chore: add profile.png
    
    * rename: rename bio.tsx
    
    * feat: add bio introdunction content

---
+++ app/_components/Bio.tsx
@@ -0,0 +1,11 @@
+export default function Bio() {
+  return (
+    <div className="bio">
+      <div className="bio-profile">profile</div>
+      <div className="bio-introduction">
+        Nerd Animals
+        <br /> No-answer를 Yes-answer로!
+      </div>
+    </div>
+  );
+}
