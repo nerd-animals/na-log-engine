fa6c961e57e750cc28c39b10ecc63e35d8e7e5de
otter <144116866+otterbits@users.noreply.github.com>
2024-02-17 14:29:30 +0900

    ♻️ css 파일을 scss로 변경 (#17)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Link from 'next/link';
-import './globals.css';
+import './globals.scss';
 
 const inter = Inter({ subsets: ['latin'] });
 
