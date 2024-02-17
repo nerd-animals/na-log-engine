b2ee1451ac820bbe149417c2a6287c8ee87eeefc
uoohoon <hoonixox@naver.com>
2024-02-17 22:19:20 +0900

    docs: add test summary

--- app/_components/PostCard.tsx
+++ app/_components/PostCard.tsx
@@ -3,6 +3,30 @@ export default function PostCard() {
     <div className="post-card-wrapper">
       <div className="post-card">
         <div className="post-card-title">test title입니다.</div>
+        <div className="post-card-summary">
+          twinkle adorable lovesick laptop miracle fascinating shine lovesick
+          twilight computer grapes hello shine pure heimish fascinating
+          fascinating flutter heimish honey girlish laptop cherish vanilla
+          seraphic honey heimish honeyrequiem blush eunoia laptop seraphic
+          destiny bijou twinkle computer eunoia cream kitten flutter adolescence
+          like requiem shine lovable world seraphic kitten purity stella iris
+          baby charming iris twinkle adolescence kitten stellalighted
+        </div>
+        <div className="post-card-info">
+          <div className="post-card-date">test date입니다.</div>
+          <div className="post-card-categories">test categories입니다.</div>
+        </div>
+      </div>
+      <div className="post-card">
+        <div className="post-card-title">test title입니다.</div>
+        <div className="post-card-summary">test summary입니다.</div>
+        <div className="post-card-info">
+          <div className="post-card-date">test date입니다.</div>
+          <div className="post-card-categories">test categories입니다.</div>
+        </div>
+      </div>
+      <div className="post-card">
+        <div className="post-card-title">test title입니다.</div>
         <div className="post-card-summary">test summary입니다.</div>
         <div className="post-card-info">
           <div className="post-card-date">test date입니다.</div>
