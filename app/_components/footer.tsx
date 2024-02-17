05a8e982d76c06e473eae0a0f362e93f9aa2e49c
uoohoon <hoonixox@naver.com>
2024-02-17 19:23:59 +0900

    remove: delete letter error file

--- app/_components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="footer-wrapper">
-      <footer className="page-footer">copyright © 2024 by NerdAnimals</footer>
-    </div>
-  );
-}
