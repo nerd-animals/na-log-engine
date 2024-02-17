fb214972fb6cf180bec10144e1060ccb3470eb07
otter <144116866+otterbits@users.noreply.github.com>
2024-02-17 16:05:29 +0900

    💄 전체 레이아웃을 중앙 정렬 (#19)
    * feat: add page-wrapper html in layout.tsx
    
    * style: set global-align center

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <div className="page-wrapper">
+          <Header />
+          {children}
+        </div>
       </body>
     </html>
   );
