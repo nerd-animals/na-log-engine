fa5897ac72051134dfdf43f7432f99a04b6c0768
otter <144116866+otterbits@users.noreply.github.com>
2024-02-17 17:29:02 +0900

    ✨ page footer component 추가 (#20)
    * feat: create page footer component
    
    * feat: show page footer component in layout.tsx
    
    * rename: upper footer component name
    
    * style: add footer style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Header from './_components/header';
+import Footer from './_components/footer';
 import './globals.scss';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -21,6 +22,7 @@ export default function RootLayout({
         <div className="page-wrapper">
           <Header />
           {children}
+          <Footer />
         </div>
       </body>
     </html>
