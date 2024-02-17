15a2a4a64390f1f52a7538f86c16fe97f3307b17
uoohoon <hoonixox@naver.com>
2024-02-17 16:39:01 +0900

    feat: create bio component

---
+++ app/_components/Bio.tsx
@@ -0,0 +1,8 @@
+export default function Bio() {
+  return (
+    <div className="bio">
+      <div className="bio-profile">profile</div>
+      <div className="bio-introduction">introduction</div>
+    </div>
+  );
+}
