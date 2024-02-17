569f2066bc3a9e4c0d52ec31b692c18265c94d45
uoohoon <hoonixox@naver.com>
2024-02-17 14:27:45 +0900

    refactor: change css to scss

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import styles from './page.module.css';
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main>
+      <div>
         <p>hello, na-log!</p>
       </div>
     </main>
