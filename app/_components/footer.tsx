374c1052bffd2d308bcd411d643824acda3ae3d4
uoohoon <hoonixox@naver.com>
2024-02-17 16:28:55 +0900

    feat: create page footer component

---
+++ app/_components/footer.tsx
@@ -0,0 +1,7 @@
+export default function footer() {
+  return (
+    <div className="footer-wrapper">
+      <footer className="page-footer">copyright © 2024 by NerdAnimals</footer>
+    </div>
+  );
+}
