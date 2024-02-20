e7fe3acecf5b46db2bcf7b6248065ab106493c7c
uoohoon <hoonixox@naver.com>
2024-02-20 16:18:08 +0900

    feat: add link to post-card

--- app/_components/PostCard.tsx
+++ app/_components/PostCard.tsx
@@ -1,20 +1,27 @@
+import Link from 'next/link';
 import { allPosts } from 'contentlayer/generated';
 
 export default function PostCard() {
   return (
     <div className="post-card-wrapper">
       {allPosts.map((post) => (
-        <div className="post-card">
-          <div className="post-card-title">{post.title}</div>
-          <div className="post-card-summary">{post.summary}</div>
-          <div className="post-card-info">
-            <div className="post-card-date">{post.date}</div>
-            <div className="post-card-categories">
-              {post.categories.map((category) => (
-                <div className="post-card-category">{category}.</div>
-              ))}
+        <div>
+          <Link
+            className="post-card"
+            href={`/post/${post.slug}`}
+            key={post._id}
+          >
+            <div className="post-card-title">{post.title}</div>
+            <div className="post-card-summary">{post.summary}</div>
+            <div className="post-card-info">
+              <div className="post-card-date">{post.date}</div>
+              <div className="post-card-categories">
+                {post.categories.map((category) => (
+                  <div className="post-card-category">{category}.</div>
+                ))}
+              </div>
             </div>
-          </div>
+          </Link>
         </div>
       ))}
     </div>
