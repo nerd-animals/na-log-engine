5d966ec4f60a12c57d1479262a35ecb5c53a2f37
otter <144116866+otterbits@users.noreply.github.com>
2024-02-20 11:33:35 +0900

    🐛 최적화 옵션 비활성화로 이미지 버그 수정 (#33)
    fix: fix image bug by unoptimizing

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
