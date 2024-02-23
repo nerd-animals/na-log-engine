e46a10ea667021d6d1a9c2725a7c66c63f37cd0d
uoohoon <hoonixox@naver.com>
2024-02-23 12:55:21 +0900

    remove: remove useless div

--- app/_components/PostCard.tsx
+++ app/_components/PostCard.tsx
@@ -5,35 +5,29 @@ export default function PostCard() {
   return (
     <div className="post-card-wrapper">
       {allPosts.map((post) => (
-        <div>
-          <Link
-            className="post-card"
-            href={`/post/${post.slug}`}
-            key={post._id}
-          >
-            <div className="post-card-title">{post.title}</div>
-            <div className="post-card-summary">{post.summary}</div>
-            <div className="post-card-info">
-              <div className="post-card-date">
-                {new Date(post.date).toLocaleDateString('en-us', {
-                  year: 'numeric',
-                  month: 'long',
-                  day: '2-digit',
-                  weekday: 'short',
-                })}
-              </div>
-              <div className="post-card-tags">
-                {post.tags
-                  ? post.tags.map((tag) => (
-                      <div className="post-card-tag" key={tag}>
-                        {tag}.
-                      </div>
-                    ))
-                  : null}
-              </div>
+        <Link className="post-card" href={`/post/${post.slug}`} key={post._id}>
+          <div className="post-card-title">{post.title}</div>
+          <div className="post-card-summary">{post.summary}</div>
+          <div className="post-card-info">
+            <div className="post-card-date">
+              {new Date(post.date).toLocaleDateString('en-us', {
+                year: 'numeric',
+                month: 'long',
+                day: '2-digit',
+                weekday: 'short',
+              })}
             </div>
-          </Link>
-        </div>
+            <div className="post-card-tags">
+              {post.tags
+                ? post.tags.map((tag) => (
+                    <div className="post-card-tag" key={tag}>
+                      {tag}.
+                    </div>
+                  ))
+                : null}
+            </div>
+          </div>
+        </Link>
       ))}
     </div>
   );
