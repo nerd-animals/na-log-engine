753cfa6f954bbbc1793a0ab11403be7963bfca54
otter <144116866+otterbits@users.noreply.github.com>
2024-02-23 20:26:30 +0900

    💄 post card ui 스타일 변경 (#67)
    * feat: output only one category from categories
    
    * style: change post card position
    
    * style: change postcard style
    
    * docs: change to 150 words or less in summary

--- app/_components/PostCard.tsx
+++ app/_components/PostCard.tsx
@@ -6,9 +6,19 @@ export default function PostCard() {
     <div className="post-card-wrapper">
       {allPosts.map((post) => (
         <Link className="post-card" href={`/post/${post.slug}`} key={post._id}>
+          <div className="post-card-categories">{post.categories.pop()}</div>
           <div className="post-card-title">{post.title}</div>
           <div className="post-card-summary">{post.summary}</div>
           <div className="post-card-info">
+            <div className="post-card-tags">
+              {post.tags
+                ? post.tags.map((tag) => (
+                    <div className="post-card-tag" key={tag}>
+                      {tag}
+                    </div>
+                  ))
+                : null}
+            </div>
             <div className="post-card-date">
               {new Date(post.date).toLocaleDateString('en-us', {
                 year: 'numeric',
@@ -17,16 +27,6 @@ export default function PostCard() {
                 weekday: 'short',
               })}
             </div>
-            <div className="post-card-tags">
-              {post.tags
-                ? post.tags.map((tag) => (
-                    <div className="post-card-tag" key={tag}>
-                      {tag}.
-                    </div>
-                  ))
-                : null}
-            </div>
-            <div className="post-card-categories">{post.categories}</div>
           </div>
         </Link>
       ))}
