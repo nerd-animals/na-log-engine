be36521d2e2dff57884e7aba3a7ef4534453742e
otter <144116866+otterbits@users.noreply.github.com>
2024-02-23 12:57:13 +0900

    ✨ map 함수에 key 일괄 추가 (#62)
    * feature: add key to map
    
    * remove: remove useless div

--- app/about/_components/TechList.tsx
+++ app/about/_components/TechList.tsx
@@ -2,7 +2,9 @@ export default function TechList({ techList }: { techList: string[] }) {
   return (
     <div className="about-tech-list">
       {techList.map((tech) => (
-        <div className="about-tech">{tech}</div>
+        <div className="about-tech" key={tech}>
+          {tech}
+        </div>
       ))}
     </div>
   );
