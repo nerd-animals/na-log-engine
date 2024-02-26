d95f8c286077561a8820063566ae5e956a1b332e
uoohoon <hoonixox@naver.com>
2024-02-26 15:48:59 +0900

    feat: show default profile.png if imagePath is none

--- app/_components/Bio.tsx
+++ app/_components/Bio.tsx
@@ -2,13 +2,14 @@ import { allBios } from 'contentlayer/generated';
 import Image from 'next/image';
 import AboutContent from 'app/about/_components/AboutContent';
 import MdxComponent from './MdxComponent';
+import defaultProfile from '../assets/default_profile.png';
 
 export default function Bio() {
   return (
     <div className="bio">
       <Image
         className="bio-profile"
-        src={allBios[0].imagePath}
+        src={allBios[0].imagePath || defaultProfile}
         alt="profile"
         width={200}
         height={200}
