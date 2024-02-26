bdd9d51c30ecee4dbf5cc82563190d561f5eecec
otter <144116866+otterbits@users.noreply.github.com>
2024-02-26 21:25:21 +0900

    ✨ bio 프로필 사진, 소개글 유무로 사용자 커스텀화 기능 추가 (#83)
    * feat: add function to get Bio data
    
    * feat: set bio to customize
    bio image 혹은 introduction, 또는 모두 제외 가능
    
    * style: set bio height
    image가 없을 때,  introduction만 있을 시 높이 고정
    
    * refactor: change 'let' to 'if'
    
    * rename: change introduction to content

--- app/_components/Bio.tsx
+++ app/_components/Bio.tsx
@@ -2,19 +2,38 @@ import { allBios } from 'contentlayer/generated';
 import Image from 'next/image';
 import MdxComponent from './MdxComponent';
 
+export function getBioData() {
+  return allBios[0];
+}
+
 export default function Bio() {
-  return (
-    <div className="bio">
-      <Image
-        className="bio-profile"
-        src={allBios[0].imagePath}
-        alt="profile"
-        width={200}
-        height={200}
-      />
-      <div className="bio-introduction">
-        <MdxComponent code={allBios[0].body.code} />
-      </div>
+  const bio = getBioData();
+
+  const bioProfile = bio.imagePath && (
+    <Image
+      className="bio-profile"
+      src={bio.imagePath}
+      alt="profile"
+      width={200}
+      height={200}
+    />
+  );
+  const bioContent = bio.body.code && (
+    <div className="bio-content">
+      <MdxComponent code={bio.body.code} />
     </div>
   );
+
+  if (bioProfile || bioContent) {
+    return (
+      <div className="bio">
+        {bioProfile}
+        {bioContent}
+      </div>
+    );
+  }
+
+  if (!bioProfile && !bioContent) {
+    return null;
+  }
 }
