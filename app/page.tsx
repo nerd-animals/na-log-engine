e5583a2645ff4cfc9174aa6e3e66ddfeca899393
uoohoon <hoonixox@naver.com>
2024-02-27 10:51:31 +0900

    refactor: unify post card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { allPosts } from 'contentlayer/generated';
 import Bio from './_components/Bio';
 import PostCard from './_components/PostCard';
 
@@ -5,8 +6,18 @@ export default function Home() {
   return (
     <>
       <Bio />
-      <main>
-        <PostCard />
+      <main className="post-card-wrapper">
+        {allPosts.map((post) => (
+          <PostCard
+            key={post._id}
+            slug={post.slug}
+            categories={post.categories}
+            title={post.title}
+            summary={post.summary}
+            tags={post.tags}
+            date={post.date}
+          />
+        ))}
       </main>
     </>
   );
