98de4a524993834611c0cdb7afaddc8c9896ecc4
SanGyuk-Raccoon <56672129+SanGyuk-Raccoon@users.noreply.github.com>
2024-03-03 01:18:42 +0900

    🐛 config.json fetch 에러 (#92)
    * remove: .env
    
    로컬 테스트시 필요할 것이라 생각했는데, 없어도 문제 없음
    
    * fix: import directly config
    
    직접 import하면 됨
    
    * refactor: modify way to get basePath

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,22 +1,5 @@
+import config from 'public/config.json';
 import Link from 'next/link';
-
-const githubRepository = process.env.GITHUB_REPOSITORY || '';
-const githubOwner = githubRepository.split('/')[0];
-const githubRepo = githubRepository.split('/')[1];
-const basePath =
-  githubRepo === `${githubOwner}.github.io` ? '/' : `/${githubRepo}`;
-
-async function getHome(): Promise<string> {
-  const fetchPath =
-    process.env.NODE_ENV === 'development'
-      ? `http://localhost:3000${basePath}`
-      : `https://${githubOwner}.github.io${basePath}`;
-
-  const res = await fetch(`${fetchPath}/config.json`);
-  const config = await res.json();
-
-  return config.title;
-}
 
 export default async function Header() {
   return (
@@ -24,7 +7,7 @@ export default async function Header() {
       <header className="page-header">
         <div className="home-section">
           <Link className="link" href="/">
-            {await getHome()}
+            {config.title}
           </Link>
         </div>
         <div className="category-section">
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,13 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import config from 'public/config.json';
 import Header from './_components/Header';
 import Footer from './_components/Footer';
 import './_styles/globals.scss';
 
 const inter = Inter({ subsets: ['latin'] });
 
-const githubRepository = process.env.GITHUB_REPOSITORY || '';
-const githubOwner = githubRepository.split('/')[0];
-const githubRepo = githubRepository.split('/')[1];
-const basePath =
-  githubRepo === `${githubOwner}.github.io` ? '/' : `/${githubRepo}`;
-
 export async function generateMetadata(): Promise<Metadata> {
-  const fetchPath =
-    process.env.NODE_ENV === 'development'
-      ? `http://localhost:3000${basePath}`
-      : `https://${githubOwner}.github.io${basePath}`;
-
-  const res = await fetch(`${fetchPath}/config.json`);
-  const config = await res.json();
-
   return {
     title: config.title,
     description: config.description,
