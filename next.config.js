2e1d3bbb65e558a3e3be95b2e3b80b2fc79b2158
SanGyuk-Raccoon <dmstjkim80@gmail.com>
2024-03-03 01:14:57 +0900

    refactor: modify way to get basePath

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
 // next.config.js
 const { withContentlayer } = require('next-contentlayer');
 
-const githubOwner = process.env.GITHUB_REPOSITORY.split('/')[0];
-const githubRepo = process.env.GITHUB_REPOSITORY.split('/')[1];
-const basePath =
-  githubRepo === `${githubOwner}.github.io` ? '/' : `/${githubRepo}`;
+function getBasePath() {
+  if (!process.env.GITHUB_REPOSITORY) {
+    return '';
+  }
+  const githubOwner = process.env.GITHUB_REPOSITORY.split('/')[0];
+  const githubRepo = process.env.GITHUB_REPOSITORY.split('/')[1];
+  const basePath =
+    githubRepo === `${githubOwner}.github.io` ? '' : `/${githubRepo}`;
+
+  return basePath;
+}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: basePath,
+  basePath: getBasePath(),
   output: 'export',
   reactStrictMode: true,
   swcMinify: true,
