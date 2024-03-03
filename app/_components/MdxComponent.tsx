09b1aff6dedfcebd34f6ac1750772d446c16c383
SanGyuk-Raccoon <56672129+SanGyuk-Raccoon@users.noreply.github.com>
2024-03-03 14:02:04 +0900

    🐛 image 경로 문제 (#94)
    * ci: install rehype-image-size to set base dir
    
    * feature: set basePath to public
    
    * ci: remove rehype-img-size
    
    * chore: remove deleted package
    
    * feature: img tag conver to Image tag

--- app/_components/MdxComponent.tsx
+++ app/_components/MdxComponent.tsx
@@ -1,4 +1,5 @@
 import { useMDXComponent } from 'next-contentlayer/hooks';
+import Image from 'next/image';
 
 interface MDXProps {
   code: string;
@@ -7,5 +8,14 @@ interface MDXProps {
 export default function MdxComponent(props: MDXProps) {
   const { code } = props;
   const MDX = useMDXComponent(code);
-  return <MDX />;
+  return (
+    <MDX
+      components={{
+        // Todo: 경고 원인 파악 필요
+        // @ts-ignore
+        // eslint-disable-next-line
+        img: () => <Image />,
+      }}
+    />
+  );
 }
