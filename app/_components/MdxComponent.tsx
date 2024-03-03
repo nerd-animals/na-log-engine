eb5fba7fe238ec5f363c496ad0f4563f5a5ef28a
SanGyuk-Raccoon <56672129+SanGyuk-Raccoon@users.noreply.github.com>
2024-03-03 14:45:00 +0900

    ✨ img to image 태그 변경 (#96)
    feature: img to image

--- app/_components/MdxComponent.tsx
+++ app/_components/MdxComponent.tsx
@@ -5,17 +5,19 @@ interface MDXProps {
   code: string;
 }
 
+const components = {
+  img: (props: any) => (
+    <Image
+      src={props.src}
+      alt={props.alt || ''}
+      width={props.width || 200}
+      height={props.height || 200}
+    />
+  ),
+};
+
 export default function MdxComponent(props: MDXProps) {
   const { code } = props;
   const MDX = useMDXComponent(code);
-  return (
-    <MDX
-      components={{
-        // Todo: 경고 원인 파악 필요
-        // @ts-ignore
-        // eslint-disable-next-line
-        img: () => <Image />,
-      }}
-    />
-  );
+  return <MDX components={components} />;
 }
