b90e3a981c169e80e49415371455ef7f2abcb9e8
SanGyuk-Raccoon <56672129+SanGyuk-Raccoon@users.noreply.github.com>
2024-03-07 21:02:46 +0900

    ✨ giscus 댓글 추가 (#110)
    * feature: giscus comment
    
    * feature: check giscus config
    
    * refactor: comment.tsx/giscus.tsx 합치기/ giscus config 조건문 변경
    
    정적 파일이라서 exist 로직 사용 불가능. config.giscus 자체에서 에러발생.
    config.giscus.active 추가해서 조건 확인
    
    * chore: remove console.log()

---
+++ app/_components/Giscus.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import config from 'public/config.json';
+import { useEffect, useRef } from 'react';
+
+function getScript() {
+  const giscusConfig = config.giscus;
+  const scriptElem = document.createElement('script');
+  const keys = Object.keys(giscusConfig);
+  const values = Object.values(giscusConfig);
+  for (let i = 0; i < keys.length; i += 1) {
+    if (keys[i] !== 'active') {
+      scriptElem.setAttribute(keys[i], values[i].toString());
+    }
+  }
+  return scriptElem;
+}
+
+export default function Giscus() {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current || ref.current.hasChildNodes()) return;
+    ref.current.appendChild(getScript());
+  }, []);
+
+  return config.giscus.active ? <section ref={ref} /> : undefined;
+}
