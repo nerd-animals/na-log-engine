9f8f527566a56a7e22c7b0b2ab8cbd8f21e13995
uoohoon <hoonixox@naver.com>
2024-03-26 15:43:20 +0900

    feat: show 'frontmatter' in write page

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -3,15 +3,33 @@
 import { useState } from 'react';
 
 export default function Write() {
-  const [content, setContent] = useState('');
+  const [post, setPost] = useState({
+    title: '',
+    author: '',
+    tags: '',
+    date: '',
+    content: '',
+  });
 
-  const onChangeInputContent = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setContent(e.target.value);
+  const onChangeInputPost = (e: any) => {
+    setPost((prevState) => ({
+      ...prevState,
+      [e.target.className]: e.target.value,
+    }));
   };
 
   return (
-    <div>
-      <textarea value={content} onChange={onChangeInputContent} />
+    <div className="write-wrapper">
+      <div className="front-matter">
+        <input className="title" type="text" onChange={onChangeInputPost} />
+        <input className="author" type="text" onChange={onChangeInputPost} />
+        <input className="tags" type="text" onChange={onChangeInputPost} />
+        <input className="date" type="date" onChange={onChangeInputPost} />
+      </div>
+      <div className="input-wrapper">
+        <textarea className="content" onChange={onChangeInputPost} />
+        <div className="preview">{post.content}</div>
+      </div>
     </div>
   );
 }
