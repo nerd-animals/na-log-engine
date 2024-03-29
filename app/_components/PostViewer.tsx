8390140a80d3fb3a912d1a1fff7c8cc4f97f5005
uoohoon <hoonixox@naver.com>
2024-03-29 19:55:36 +0900

    refactor: change 'write-preview' div dir(write page->post viewer)

--- app/_components/PostViewer.tsx
+++ app/_components/PostViewer.tsx
@@ -4,9 +4,11 @@ import PostHeader from './PostHeader';
 
 export default function PostViewer({ post }: { post: PostWithoutSlug }) {
   return (
-    <div className="post-wrapper">
-      <PostHeader data={post.frontMatter} />
-      <PostContent content={post.content} />
+    <div className="write-preview">
+      <div className="post-wrapper">
+        <PostHeader data={post.frontMatter} />
+        <PostContent content={post.content} />
+      </div>
     </div>
   );
 }
