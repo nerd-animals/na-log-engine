b04ad3bb7ddb6112d5ca18cd6d3d824f611d5b75
otter <144116866+otterbits@users.noreply.github.com>
2024-04-03 16:20:14 +0900

    💄 post/[...slug] page Card UI 제거 (#141)
    * style: apply preview style only in 'write' page preview
    
    * style: apply post header style
    
    * style: apply post content style

--- app/_components/post/PostViewer.tsx
+++ app/_components/post/PostViewer.tsx
@@ -4,11 +4,9 @@ import PostContent from './PostContent';
 
 export default function PostViewer({ post }: { post: PostWithoutSlug }) {
   return (
-    <div className="write-preview">
-      <div className="post-wrapper">
-        <PostHeader data={post.frontMatter} />
-        <PostContent content={post.content} />
-      </div>
+    <div className="post-wrapper">
+      <PostHeader data={post.frontMatter} />
+      <PostContent content={post.content} />
     </div>
   );
 }
