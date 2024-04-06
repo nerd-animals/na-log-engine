a8251e3b6b61fbd8f7d79ef6759f39ac666ae1b4
otter <144116866+otterbits@users.noreply.github.com>
2024-04-06 18:04:34 +0900

    💬 function 이름을 Section에서 AboutSection으로 변경 (#144)
    rename: funtion name 'Section to AboutSection'
    
    Co-authored-by: SanGyuk-Raccoon <56672129+SanGyuk-Raccoon@users.noreply.github.com>

--- app/_components/custom/AboutSection.tsx
+++ app/_components/custom/AboutSection.tsx
@@ -2,7 +2,8 @@ import DividerTitle from '@/_components/design/DividerTitle';
 import MdxContent from '@/_components/mdx/MdxContent';
 import { About } from 'lib/AboutManager';
 
-export default function Section({ about }: { about: About }) {
+
+export default function AboutSection({ about }: { about: About }) {
   return (
     <div className="about-section">
       <DividerTitle title={about.title} />
