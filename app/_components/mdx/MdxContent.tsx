24ea787afea680f5c183731af381514aa40a638f
raccoon <dmstjkim80@gmail.com>
2024-04-06 19:09:15 +0900

    feature: use unified in MdxContent

--- app/_components/mdx/MdxContent.tsx
+++ app/_components/mdx/MdxContent.tsx
@@ -1,3 +1,16 @@
-export default function MdxContent({ content }: { content: string }) {
-  return content;
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeSanitize from 'rehype-sanitize'
+import rehypeStringify from 'rehype-stringify'
+
+export default async function MdxContent({ content }: { content: string }) {
+  const html = await unified()
+    .use(remarkParse) // Convert into markdown AST
+    .use(remarkRehype) // Transform to HTML AST
+    .use(rehypeSanitize) // Sanitize HTML input
+    .use(rehypeStringify) // Convert AST into serialized HTML
+    .process(content)
+ 
+  return <div dangerouslySetInnerHTML={{ __html: html.toString() }} />
 }
