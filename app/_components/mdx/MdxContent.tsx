0ff35c1dad89a20243a0bb83ce9f1d6a1f0baabe
uoohoon <hoonixox@naver.com>
2024-04-09 19:18:35 +0900

    feat: apply rehype-pretty-code plugin

--- app/_components/mdx/MdxContent.tsx
+++ app/_components/mdx/MdxContent.tsx
@@ -1,24 +1,35 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeRaw from 'rehype-raw';
 import rehypeStringify from 'rehype-stringify';
+import rehypePrettycode from 'rehype-pretty-code';
 import remarkGfm from 'remark-gfm';
 import remarkBreaks from 'remark-breaks';
 
 export default function MdxContent({ content }: { content: string }) {
-  const html = unified()
-    .use(remarkParse)
-    .use(remarkGfm)
-    .use(remarkBreaks)
-    // 여기다가 remark plugin을 추가
-    .use(remarkRehype, { allowDangerousHtml: true })
-    .use(rehypeRaw)
-    // 여기다가 rehype plugin을 추가
-    .use(rehypeStringify)
-    .processSync(content);
+  const [htmlContent, setHtmlContent] = useState('');
 
-  return <div dangerouslySetInnerHTML={{ __html: html.toString() }} />;
+  useEffect(() => {
+    async function processContent() {
+      const html = await unified()
+        .use(remarkParse)
+        .use(remarkGfm)
+        .use(remarkBreaks)
+        // 여기다가 remark plugin을 추가
+        .use(remarkRehype, { allowDangerousHtml: true })
+        .use(rehypeRaw)
+        // 여기다가 rehype plugin을 추가
+        .use(rehypeStringify)
+        .use(rehypePrettycode)
+        .process(content);
+
+      setHtmlContent(html.toString());
+    }
+    processContent();
+  }, [content]);
+  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
 }
