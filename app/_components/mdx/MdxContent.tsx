b26517800067b135d1919273102889b786293ffd
SanGyuk-Raccoon <56672129+SanGyuk-Raccoon@users.noreply.github.com>
2024-04-11 13:34:24 +0900

    ✨ unified() 를 이용한 마크다운 content 렌더링 (#145)
    * ci: unified library
    
    * feature: use unified in MdxContent
    
    * ci: remark&rehype plugin
    
    * feature: unified()
    
    * CI: install rehype pretty code
    
    * feat: apply rehype-pretty-code plugin
    
    * feat: apply rehype-slug plugin
    
    * feat: apply rehpye-autolink-headings plugin
    
    * feat: apply rehype-external-links plugin
    
    * chore: delete comment
    
    * ci: delete unused plug-in
    
    ---------
    
    Co-authored-by: uoohoon <hoonixox@naver.com>

--- app/_components/mdx/MdxContent.tsx
+++ app/_components/mdx/MdxContent.tsx
@@ -1,3 +1,50 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeRaw from 'rehype-raw';
+import rehypeStringify from 'rehype-stringify';
+import rehypePrettycode from 'rehype-pretty-code';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeExternalLinks from 'rehype-external-links';
+import remarkGfm from 'remark-gfm';
+import remarkBreaks from 'remark-breaks';
+
 export default function MdxContent({ content }: { content: string }) {
-  return content;
+  const [htmlContent, setHtmlContent] = useState('');
+
+  useEffect(() => {
+    async function processContent() {
+      const html = await unified()
+        .use(remarkParse)
+        .use(remarkGfm)
+        .use(remarkBreaks)
+        .use(remarkRehype, { allowDangerousHtml: true })
+        .use(rehypeRaw)
+        .use(rehypeStringify)
+        .use(rehypePrettycode)
+        .use(rehypeSlug)
+        .use(rehypeAutolinkHeadings, {
+          properties: {
+            className: ['anchor'],
+          },
+          behavior: 'wrap',
+        })
+        .use(rehypeExternalLinks, {
+          properties: {
+            class: 'external-link',
+          },
+          target: '_blank',
+          rel: ['noopener noreferrer'],
+        })
+        .process(content);
+
+      setHtmlContent(html.toString());
+    }
+    processContent();
+  }, [content]);
+  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
 }
