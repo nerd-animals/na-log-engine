878c53386852f7af32ebb71bcf415e139512cd4f
uoohoon <hoonixox@naver.com>
2024-04-11 21:57:37 +0900

    feat: create useDownloadMdx custom hook

---
+++ app/_hooks/useDownloadMdx.ts
@@ -0,0 +1,28 @@
+import { useContext } from 'react';
+import { PostContext } from '@/_context/PostContext';
+
+export default function useDownloadMdx() {
+  const { post } = useContext(PostContext);
+
+  const DownloadMdx = () => {
+    const mdxContent = `---
+title: '${post.frontMatter.title}'
+tags: [${post.frontMatter.tags.map((tag) => `'${tag}'`).join(', ')}]
+author: '${post.frontMatter.author}'
+---
+    
+${post.content}`;
+
+    const fileName = 'mdx.mdx';
+    const element = document.createElement('a');
+    const file = new Blob([mdxContent], {
+      type: 'text/plain',
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = fileName;
+    document.body.appendChild(element);
+    element.click();
+    element.remove();
+  };
+  return DownloadMdx;
+}
