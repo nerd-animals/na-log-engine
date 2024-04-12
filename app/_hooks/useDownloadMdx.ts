95e8fb3b3a9ec54250a56426c67896a45409e471
uoohoon <hoonixox@naver.com>
2024-04-12 09:24:09 +0900

    refactor: specify tags as formattedTags

--- app/_hooks/useDownloadMdx.ts
+++ app/_hooks/useDownloadMdx.ts
@@ -5,13 +5,16 @@ export default function useDownloadMdx() {
   const { post } = useContext(PostContext);
 
   const DownloadMdx = () => {
+    const formattedTags: string = post.frontMatter.tags
+      .map((tag) => `'${tag}'`)
+      .join(', ');
     const formattedDate: string = post.frontMatter.date
       .toISOString()
       .substring(0, 10);
 
     const mdxContent = `---
 title: '${post.frontMatter.title}'
-tags: [${post.frontMatter.tags.map((tag) => `'${tag}'`).join(', ')}]
+tags:  [${formattedTags}]
 author: '${post.frontMatter.author}'
 date: ${formattedDate}
 ---
