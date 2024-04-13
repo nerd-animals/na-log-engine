88336a9ff7c19f98c489e9f957d7b266a4207c10
uoohoon <hoonixox@naver.com>
2024-04-13 17:34:45 +0900

    feat: add Header component in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Bio from '@/_components/custom/Bio';
 import PostCard from '@/_components/post/PostCard';
+import Header from '@/_components/layout/Header';
 import PostManager from 'lib/PostManager';
 
 export default function Home() {
   const allPosts = PostManager.getInstance().getAllPost();
   return (
     <>
+      <Header />
       <Bio />
       <main className="post-card-wrapper">
         {allPosts.map((post) => (
