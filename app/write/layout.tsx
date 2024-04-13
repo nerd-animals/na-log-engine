7455d21469a97c16c8861041b17e73c7abea4084
uoohoon <hoonixox@naver.com>
2024-04-13 17:35:20 +0900

    feat: add write page layout

---
+++ app/write/layout.tsx
@@ -0,0 +1,7 @@
+export default function WriteLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="write-wrapper">{children}</div>;
+}
--- app/write/page.tsx
+++ app/write/page.tsx
@@ -9,12 +9,12 @@ function WriteContent() {
   const { post } = useContext(PostContext);
 
   return (
-    <div className="write-wrapper">
+    <>
       <PostEditor />
       <div className="write-preview">
         <PostViewer post={post} />
       </div>
-    </div>
+    </>
   );
 }
 
