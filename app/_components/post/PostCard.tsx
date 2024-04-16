b17b07377f8e9ae8f9305912d8cf9767e3a68679
otter <144116866+otterbits@users.noreply.github.com>
2024-04-16 18:10:22 +0900

    💄 글쓰기 페이지 ui 수정 및 전체 페이지 html 구조 변경 (#154)
    * refactor: change file extension(tsx->ts)
    
    * refactor: add contents routing group folder
    
    * feat: add contents layout
    
    * refactor: change strcture in root layout
    
    * feat: add Header component in home page
    
    * feat: add write page layout
    
    * feat: add post-content div
    
    * feat: create 'write navigation' component
    
    * feat: apply 'WriteNav' in PostEditor
    
    * feat: create info-wrapper div
    
    * feat: create content-wrapper div
    
    * style: change toast style(hidden->none)
    
    * style: write page style
    
    * style: navigation style
    
    * refactor: change html semantic elements
    
    * style: apply html semantic tag change point
    
    * feat: change toast position
    
    * style: add x-large device width
    
    * style: write-page height
    
    * style: write page responsive webpage
    
    * style: editor date, tags flex-grow
    
    * style: preview padding

--- app/_components/post/PostCard.tsx
+++ app/_components/post/PostCard.tsx
@@ -9,31 +9,33 @@ export default function PostCard({
   const { title, summary, tags, date, slug } = frontMatter;
   const categories = slug.slice(0, -1);
   return (
-    <Link className="post-card" href={`/post/${slug.join('/')}`}>
-      <div className="post-card-category">
-        {categories[categories.length - 1]}
-      </div>
-      <div className="post-card-title">{title}</div>
-      <div className="post-card-summary">{summary}</div>
-      <div className="post-card-info">
-        <div className="post-card-tags">
-          {tags
-            ? tags.slice(0, 3).map((tag) => (
-                <div className="post-card-tag" key={tag}>
-                  {tag}
-                </div>
-              ))
-            : null}
+    <div className="post-card-wrapper">
+      <Link className="post-card" href={`/post/${slug.join('/')}`}>
+        <div className="post-card-category">
+          {categories[categories.length - 1]}
         </div>
-        <div className="post-card-date">
-          {new Date(date).toLocaleDateString('en-us', {
-            year: 'numeric',
-            month: 'long',
-            day: '2-digit',
-            weekday: 'short',
-          })}
+        <div className="post-card-title">{title}</div>
+        <div className="post-card-summary">{summary}</div>
+        <div className="post-card-info">
+          <div className="post-card-tags">
+            {tags
+              ? tags.slice(0, 3).map((tag) => (
+                  <div className="post-card-tag" key={tag}>
+                    {tag}
+                  </div>
+                ))
+              : null}
+          </div>
+          <div className="post-card-date">
+            {new Date(date).toLocaleDateString('en-us', {
+              year: 'numeric',
+              month: 'long',
+              day: '2-digit',
+              weekday: 'short',
+            })}
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
