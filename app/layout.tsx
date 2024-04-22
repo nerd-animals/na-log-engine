98ef34631d8057fed8e9a3f1dd18526acadccfdb
SanGyuk-Raccoon <56672129+SanGyuk-Raccoon@users.noreply.github.com>
2024-04-22 18:25:19 +0900

    ✨ Meta Tag 추가 (#159)
    * feat: meta tag in post page
    
    * feat: add og meta tag in layout
    
    * feat: viewport meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,24 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import config from 'public/config.json';
 import Footer from '@/_components/layout/Footer';
 import '@/_styles/globals.scss';
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  minimumScale: 2,
+  maximumScale: 0.5,
+  userScalable: true,
+};
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
     title: config.title,
     description: config.description,
+    openGraph: {
+      title: config.title,
+      description: config.description,
+    },
   };
 }
 
