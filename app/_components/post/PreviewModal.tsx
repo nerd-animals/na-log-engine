50a1c69d79c421d0ff06f656d65110fcbffaaf93
uoohoon <hoonixox@naver.com>
2024-04-24 23:26:09 +0900

    feat: create PreviewModal

---
+++ app/_components/post/PreviewModal.tsx
@@ -0,0 +1,9 @@
+export default function PreviewModal() {
+  return (
+    <div className="preview-modal-wrapper">
+      <div className="preview-modal">
+        <input type="text" />
+      </div>
+    </div>
+  );
+}
