42547502d025cf386317fadb61ce23c8345ba6ca
uoohoon <hoonixox@naver.com>
2024-04-26 10:49:56 +0900

    feat: apply updateSummary

--- app/_components/post/PreviewModal.tsx
+++ app/_components/post/PreviewModal.tsx
@@ -1,8 +1,18 @@
+import { PostContext } from '@/_context/PostContext';
+import { useContext } from 'react';
+
 export default function PreviewModal() {
+  const { post, updateSummary } = useContext(PostContext);
+
   return (
     <div className="preview-modal-wrapper">
       <div className="preview-modal">
-        <input type="text" />
+        <input
+          className="summary"
+          type="text"
+          value={post.frontMatter.summary}
+          onChange={updateSummary}
+        />
       </div>
     </div>
   );
