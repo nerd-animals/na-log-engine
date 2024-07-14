1b10c6535a784042c1a67c2abc3f57ad245a570d
uoohoon <hoonixox@naver.com>
2024-07-14 20:51:59 +0900

    feat: pass initialPosts to PostSearch as props in Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
 import Bio from '@/_components/custom/Bio';
 import Header from '@/_components/layout/Header';
 import PostTab from '@/_components/post/PostTab';
-import PostSearch from '@/_components/post/PostSearch';
 import PostManager from 'lib/PostManager';
 
 export default function Home() {
   const allPosts = PostManager.getInstance().getAllPost();
   return (
     <>
-      <Header />
+      <Header initialPosts={allPosts} />
       <main className="home-wrapper">
         <Bio />
         <div className="post-container">
-          <PostSearch />
           <PostTab initialPosts={allPosts} />
         </div>
       </main>
