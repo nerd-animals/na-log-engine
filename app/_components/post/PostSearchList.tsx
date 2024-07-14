eabc31e87214d0f1dca62f5eb3d861739d65d50b
uoohoon <hoonixox@naver.com>
2024-07-14 21:18:54 +0900

    feat: display post links in PostSearchList

--- app/_components/post/PostSearchList.tsx
+++ app/_components/post/PostSearchList.tsx
@@ -1,3 +1,17 @@
-export default function PostSearchList() {
-  return;
+import Link from 'next/link';
+
+export default function PostSearchList({
+  slug,
+  title,
+}: {
+  slug: string;
+  title: string;
+}) {
+  return (
+    <div>
+      <Link href={`/post/${slug}`}>
+        <div>{title}</div>
+      </Link>
+    </div>
+  );
 }
