70d9ccbbf00940849d7c2591a873fbac6e03fd8b
uoohoon <hoonixox@naver.com>
2024-07-14 21:00:09 +0900

    feat: manage search post results as state

--- app/_components/post/PostSearch.tsx
+++ app/_components/post/PostSearch.tsx
@@ -1,22 +1,42 @@
 'use client';
 
 import { useState } from 'react';
+import { Post } from 'lib/PostManager';
 import PostSearchList from '@/_components/post/PostSearchList';
 
-export default function PostSearch() {
+export default function PostSearch({ initialPosts }: { initialPosts: Post[] }) {
+  const posts = initialPosts;
   const [searchedPostTitle, setSearchedPostTitle] = useState('');
-  const handleChangeInputSearchedPostTitle = (e) => {
+
+  const handleChangeInputSearchedPostTitle = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setSearchedPostTitle(e.target.value);
   };
+
+  const searchedPosts = searchedPostTitle
+    ? posts.filter((post) => post.frontMatter.title.includes(searchedPostTitle))
+    : [];
 
   return (
     <>
       <input
         className="search"
         type="text"
+        value={searchedPostTitle}
         onChange={handleChangeInputSearchedPostTitle}
       />
-      <PostSearchList />
+      {searchedPosts.length > 0 ? (
+        searchedPosts.map((post) => (
+          <PostSearchList
+            key={post.frontMatter.slug.join('/')}
+            slug={post.frontMatter.slug.join('/')}
+            title={post.frontMatter.title}
+          />
+        ))
+      ) : (
+        <div>해당하는 글이 없습니다.</div>
+      )}
     </>
   );
 }
