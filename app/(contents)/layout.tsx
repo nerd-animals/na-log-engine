cbac9427306f9483c0f716ccf00a733e53e9e621
uoohoon <hoonixox@naver.com>
2024-07-16 00:27:11 +0900

    fix: build error by adding props to the header in blog layout

--- app/(contents)/layout.tsx
+++ app/(contents)/layout.tsx
@@ -1,3 +1,4 @@
+import PostManager from 'lib/PostManager';
 import Header from '@/_components/layout/Header';
 
 export default function BlogLayout({
@@ -5,9 +6,10 @@ export default function BlogLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const allPosts = PostManager.getInstance().getAllPost();
   return (
     <>
-      <Header />
+      <Header initialPosts={allPosts} />
       <main className="contents-wrapper">{children}</main>
     </>
   );
