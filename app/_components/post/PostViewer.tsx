09bd6886dd30335c124bca9757efa92149f6b842
uoohoon <hoonixox@naver.com>
2024-08-22 00:24:07 +0900

    feat: memo postheader, MdxContent

--- app/_components/post/PostViewer.tsx
+++ app/_components/post/PostViewer.tsx
@@ -1,13 +1,17 @@
 import { Post } from 'lib/PostManager';
+import { memo } from 'react';
 import MdxContent from '@/_components/mdx/MdxContent';
 import PostHeader from './PostHeader';
+
+const MemoizedPostHeader = memo(PostHeader);
+const MemoizedMdxContent = memo(MdxContent);
 
 export default function PostViewer({ post }: { post: Post }) {
   return (
     <div className="post-wrapper">
-      <PostHeader data={post.frontMatter} />
+      <MemoizedPostHeader data={post.frontMatter} />
       <div className="post-content">
-        <MdxContent content={post.content} />
+        <MemoizedMdxContent content={post.content} />
       </div>
     </div>
   );
