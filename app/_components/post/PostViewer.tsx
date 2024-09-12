e4a0552cb2de38c70b3b0675942e72aa7513d075
uoohoon <hoonixox@naver.com>
2024-09-12 22:38:20 +0900

    feat: delete memo in postviewer

--- app/_components/post/PostViewer.tsx
+++ app/_components/post/PostViewer.tsx
@@ -1,17 +1,13 @@
 import { Post } from 'lib/PostManager';
-import { memo } from 'react';
 import MdxContent from '@/_components/mdx/MdxContent';
 import PostHeader from './PostHeader';
-
-const MemoizedPostHeader = memo(PostHeader);
-const MemoizedMdxContent = memo(MdxContent);
 
 export default function PostViewer({ post }: { post: Post }) {
   return (
     <div className="post-wrapper">
-      <MemoizedPostHeader data={post.frontMatter} />
+      <PostHeader data={post.frontMatter} />
       <div className="post-content">
-        <MemoizedMdxContent content={post.content} />
+        <MdxContent content={post.content} />
       </div>
     </div>
   );
